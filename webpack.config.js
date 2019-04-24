8f7f1aae381b4dd647420c3ada4b13fa31d801cd
Becky Rush <becky.rush@bbc.co.uk>
2019-04-24 11:48:55 -0700

    handle images

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,10 @@ module.exports = {
                     },
                     'css-loader',
                 ],
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader?limit=100000'
             }
        ],
     },
